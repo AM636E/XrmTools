9dfa8e8d967c66c3b9368e0a1d1eeff840df305a
Nazarii Brenchuk <bnazariy@gmail.com>
2015-10-13 22:38:06 +0300

    Updated scripts

--- restore.ps1
+++ restore.ps1
@@ -1,11 +1,16 @@
-ForEach ($item in (Get-ChildItem -Path "./src" -Directory)) {
-	Write-Host "$($item.FullName)\project.json"
-	if(Test-Path "$($item.FullName)\project.json") {
-		Set-Location $item.FullName
-		if(Test-Path "./bin") {
-			Remove-Item -Recurse -Force "./bin"
+Function Restore($baseFolder) {
+	ForEach ($item in (Get-ChildItem -Path $baseFolder -Directory)) {
+		Write-Host "$($item.FullName)\project.json"
+		if(Test-Path "$($item.FullName)\project.json") {
+			Set-Location $item.FullName
+			if(Test-Path "./bin") {
+				Remove-Item -Recurse -Force "./bin"
+			}
+			dnu restore > $null
+			Set-Location ../../
 		}
-		dnu restore > $null
-		Set-Location ../../
 	}
 }
+
+Restore("./src")
+Restore("./tests")

be24771d36e973e5ca5ea8605dc48c47053ca568
Nazarii Brenchuk <bnazariy@gmail.com>
2015-10-27 17:27:39 +0200

    Added test for excluded

--- tests/DynamicaLabs.XrmTools.Tests/TestUtilsTests.cs
+++ tests/DynamicaLabs.XrmTools.Tests/TestUtilsTests.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using DynamicaLabs.XrmTools.Construction;
 using DynamicaLabs.XrmTools.Testing;
 using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
 using Xunit;
 
 namespace DynamicaLabs.XrmTools.Tests
 {
     public class TestUtilsTests
     {
-        [Fact]
-        public void TestNoException()
+        private Entity CreatEntity()
         {
-            var entity = new Entity
+            return new Entity
             {
                 Attributes =
                 {
@@ -24,6 +24,11 @@ namespace DynamicaLabs.XrmTools.Tests
                     new KeyValuePair<string, object>("optionset", new OptionSetValue(100))
                 }
             };
+        }
+        [Fact]
+        public void TestNoException()
+        {
+            var entity = CreatEntity();
             var constructor = new ReflectionEntityConstructor();
             var obj = constructor.ConstructObject<TestModel>(entity);
             TestUtils.AssertEqual(obj, entity);
@@ -32,18 +37,7 @@ namespace DynamicaLabs.XrmTools.Tests
         [Fact]
         public void TestWithException()
         {
-            var entity = new Entity
-            {
-                Attributes =
-                {
-                    new KeyValuePair<string, object>("crmid", Guid.Parse("FD93433F-694E-4B30-9B7E-668B3E53A8E5")),
-                    new KeyValuePair<string, object>("name", "Gordon"),
-                    new KeyValuePair<string, object>("surname", "Shumway"),
-                    new KeyValuePair<string, object>("entityreference",
-                        new EntityReference("", Guid.Parse("FD93433F-694E-4B30-9B7E-668B3E53A8E5"))),
-                    new KeyValuePair<string, object>("optionset", new OptionSetValue(100))
-                }
-            };
+            var entity = CreatEntity();
             var constructor = new ReflectionEntityConstructor();
             var obj = constructor.ConstructObject<TestModel>(entity);
             obj.Name = "exg;lksrg";
@@ -53,6 +47,15 @@ namespace DynamicaLabs.XrmTools.Tests
                 $"Assertion failed. Property Name = {obj.Name}. Entity name = {entity["name"]}",
                 ex.Message
                 );
+        }
+
+        [Fact]
+        public void TestWithExcluded()
+        {
+            var entity = CreatEntity();
+            var obj = new ReflectionEntityConstructor().ConstructObject<TestModel>(entity);
+            obj.Name = "nadia";
+            TestUtils.AssertEqual(obj, entity, new[] { "name" });
         }
     }
 }

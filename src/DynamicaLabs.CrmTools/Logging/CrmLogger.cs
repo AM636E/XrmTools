a5d190e5ced6cfc7a9d15227143483571f9575a8
Nazarii Brenchuk <bnazariy@gmail.com>
2016-04-08 16:08:28 +0300

    Added logging

---
+++ src/DynamicaLabs.CrmTools/Logging/CrmLogger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DynamicaLabs.Tools.Logging;
+using Microsoft.Xrm.Sdk;
+
+namespace DynamicaLabs.CrmTools.Logging
+{
+    /// <summary>
+    /// Logger with crm backend.
+    /// </summary>
+    public sealed class CrmLogger : BaseLogger
+    {
+        private readonly IOrganizationServiceFactory _osFactory;
+        private readonly CrmLoggerSettings _settings;
+
+        /// <summary>
+        /// Initialize a new instance of CrmLogger.
+        /// </summary>
+        /// <param name="osFactory">Factory to get OrganizationService.</param>
+        /// <param name="settings">Settings <see cref="CrmLoggerSettings"/></param>
+        public CrmLogger(IOrganizationServiceFactory osFactory, CrmLoggerSettings settings)
+        {
+            _osFactory = osFactory;
+            _settings = settings;
+        }
+
+        public override void Log(LogType logType, string message)
+        {
+            var service = _osFactory.CreateOrganizationService(null);
+
+            var log = new Entity(_settings.LogEntityName)
+            {
+                [_settings.MessageField] = message
+            };
+            // Set log type to entity.
+            _settings.LogTypeSetter?.Invoke(logType, log);
+            service.Create(log);
+            (service as IDisposable)?.Dispose();
+        }
+    }
+}
---
+++ src/DynamicaLabs.CrmTools/Logging/CrmLoggerSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DynamicaLabs.Tools.Logging;
+using Microsoft.Xrm.Sdk;
+
+namespace DynamicaLabs.CrmTools.Logging
+{
+    /// <summary>
+    /// Settings for CrmLogger.
+    /// </summary>
+    public class CrmLoggerSettings
+    {
+        /// <summary>
+        /// EntityLogicalName of log holder. Required.
+        /// </summary>
+        public string LogEntityName { get; set; }
+
+        /// <summary>
+        /// Field for subject. Required.
+        /// </summary>
+        public string SubjectField { get; set; }
+
+        /// <summary>
+        /// Field for message. Required.
+        /// </summary>
+        public string MessageField { get; set; }
+
+        /// <summary>
+        /// Action to fill log type. Optional.
+        /// </summary>
+        public Action<LogType, Entity> LogTypeSetter { get; set; }
+    }
+}
